d9c927849afd4a6b6decd63a7640ff4b714e0f81
junyoung4213 <junyoung4213@gmail.com>
2021-07-12 02:37:09 +0900

    OAuth2 이용한 구글 로그인 구현

---
+++ src/main/java/kr/dowajwo/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,17 @@
+package kr.dowajwo.springboot.config.auth.dto;
+
+import kr.dowajwo.springboot.domain.user.User;
+import lombok.Getter;
+
+@Getter
+public class SessionUser {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
--- src/main/java/kr/dowajwo/springboot/web/IndexController.java
+++ src/main/java/kr/dowajwo/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package kr.dowajwo.springboot.web;
 
+import kr.dowajwo.springboot.config.auth.dto.SessionUser;
 import kr.dowajwo.springboot.service.posts.PostsService;
 import kr.dowajwo.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +10,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts",postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName",user.getName());
+        }
         return "index";
     }
 
