7f27a711751763165c02b06ecaaa9573be24b391
junyoung4213 <junyoung4213@gmail.com>
2021-07-11 21:00:38 +0900

    글 조회목록 구현

--- src/main/java/kr/dowajwo/springboot/service/posts/PostsService.java
+++ src/main/java/kr/dowajwo/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package kr.dowajwo.springboot.service.posts;
 
 import kr.dowajwo.springboot.domain.posts.Posts;
 import kr.dowajwo.springboot.domain.posts.PostsRepository;
+import kr.dowajwo.springboot.web.dto.PostsListResponseDto;
 import kr.dowajwo.springboot.web.dto.PostsResponseDto;
 import kr.dowajwo.springboot.web.dto.PostsSaveRequestDto;
 import kr.dowajwo.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.stream.Collectors;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +38,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
